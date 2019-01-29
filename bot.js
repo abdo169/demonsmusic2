986f03f5da0bcd788cc2c84463ae62110ecf30a4
abdo169 <44899237+abdo169@users.noreply.github.com>
2019-01-29 04:55:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -316,7 +316,7 @@ client.on('message', message => {
     if (message.content === '2help') {
         let helpEmbed = new Discord.RichEmbed()
         .setTitle('**أوامر الميوزك...**')
-        .setDescription('**برفكس البوت (4)**')
+        .setDescription('**برفكس البوت (2)**')
         .addField('play', 'لتشغيل اغنية')
         .addField('disconnect', 'الخروج من رومك الصوتي')
         .addField('skip', 'تخطي الأغنية')
@@ -329,7 +329,7 @@ client.on('message', message => {
 });
 
 const adminprefix = "2";
-const devs = ['459133035397513228'];
+const devs = ['487313531071496195'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
@@ -360,7 +360,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Demons | Music 2 `)//حقوق دايموند كودز
+client.user.setGame(`CHICAGO | Music 2 `)//حقوق دايموند كودز
 client.user.setStatus("online")
 });
 
